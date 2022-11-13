8258e0519515e5708d1f8a62e876583205103c6e
iclal feyza <iclalfeyzaozeken@hotmail.com>
2022-11-13 22:49:09 +0300

    l

--- src/harmony.java
+++ src/harmony.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class harmony {
+import java.util.Scanner;
+
+public class harmony {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Bir sayı giriniz: ");
+        double n = inp.nextInt();
+        double a = 0;
+        double b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            b /= i;
+            a += b;
+            b = 1;
+            System.out.println(a);
+        }
+    }
 }
